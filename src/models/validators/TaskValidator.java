7df64f92945ca1c0d9c70aa17836523238626242
takuya.tsukahara <TAKUYA.TSUKAHARA@grp.tmnf.jp>
2022-05-25 14:57:55 +0900

    バリデーションを追加

---
+++ src/models/validators/TaskValidator.java
@@ -0,0 +1,42 @@
+package models.validators;
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Task;
+public class TaskValidator {
+    // バリデーションを実行する
+    public static List<String> validate(Task m) {
+        List<String> errors = new ArrayList<String>();
+
+        String title_error = validateTitle(m.getTitle());
+        if(!title_error.equals("")) {
+            errors.add(title_error);
+        }
+
+        String content_error = validateContent(m.getContent());
+        if(!content_error.equals("")) {
+            errors.add(content_error);
+        }
+
+        return errors;
+    }
+
+    // タスク名の必須入力チェック
+    private static String validateTitle(String title) {
+        if(title == null || title.equals("")) {
+            return "タスク名を入力してください。";
+        }
+
+        return "";
+    }
+
+    // タスク内容の必須入力チェック
+    private static String validateContent(String content) {
+        if(content == null || content.equals("")) {
+            return "タスク内容を入力してください。";
+        }
+
+        return "";
+    }
+
+}
