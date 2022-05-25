22cb8c3d9321bbdddd895325b2d341a1324295c8
takuya.tsukahara <TAKUYA.TSUKAHARA@grp.tmnf.jp>
2022-05-25 13:10:04 +0900

    getAllTasksを追加

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -4,7 +4,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class DBUtil {
-    private static final String PERSISTENCE_UNIT_NAME = "message_board";
+    private static final String PERSISTENCE_UNIT_NAME = "tasklist";
     private static EntityManagerFactory emf;
 
     public static EntityManager createEntityManager() {
